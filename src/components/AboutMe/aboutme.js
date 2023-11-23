549133718fd2604e7ed25483f648c529ee44f4a3
kwaters3 <knickler3@gmail.com>
2023-11-23 09:42:34 -0500

    updated about me section

--- src/components/AboutMe/aboutme.js
+++ src/components/AboutMe/aboutme.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './aboutme.css';
-import UIDesign from '../../assets/ui-design.png';
-import WebDesign from '../../assets/website-design.png';
-import AppDesign from '../../assets/app-design.png';
+import FullStack from '../../assets/fullstack.png';
+import FrontEnd from '../../assets/frontend.png';
+import BackEnd from '../../assets/backend.png';
 
 const AboutMe = () => {
   return (
@@ -15,27 +15,27 @@ const AboutMe = () => {
           </div>
           <div className='skillBars'>
             <div className='skillBar'>
-              <img src={UIDesign} alt='UI design' className='skillBarImg'/>
+              <img src={FullStack} alt='UI design' className='skillBarImg'/>
               <div className='skillBarText'>
-                <h2>UI Design</h2>
+                <h2>Full Stack</h2>
                 <p>As a skilled UI designer, I excel in creating visually stunning and user-friendly interfaces. Proficient in cutting-edge design tools, I'm committed to continuous learning, staying at the forefront of UI design to tackle new challenges and push digital aesthetics boundaries.</p>
           </div>
           </div>
           </div>
           <div className='skillBars'>
             <div className='skillBar'>
-              <img src={WebDesign} alt='Web Design' className='skillBarImg'/>
+              <img src={FrontEnd} alt='Web Design' className='skillBarImg'/>
               <div className='skillBarText'>
-                <h2>Web Design</h2>
+                <h2>Front - End</h2>
                 <p>As a web designer, I create visually appealing and functional websites using the latest design tools, staying ahead of trends to meet evolving user expectations.</p>
           </div>
           </div>
           </div>
           <div className='skillBars'>
             <div className='skillBar'>
-              <img src={AppDesign} alt='App Design' className='skillBarImg'/>
+              <img src={BackEnd} alt='App Design' className='skillBarImg'/>
               <div className='skillBarText'>
-                <h2>App Design</h2>
+                <h2>Back - End</h2>
                 <p>As an app designer, with a passion for crafting visually stunning, user-centric applications. Proficient in cutting-edge design tools, I bring creativity and functionality to every project.</p>
           </div>
           </div>
