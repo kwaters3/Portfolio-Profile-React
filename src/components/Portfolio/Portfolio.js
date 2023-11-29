1982c9ff6f23db5c48e7f8ef865c424c37c845d0
kwaters3 <knickler3@gmail.com>
2023-11-29 15:38:02 -0500

    added link emojis to project

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,6 +4,9 @@ import Portfolio1 from '../../assets/portfolio-1.png';
 import Portfolio2 from '../../assets/portfolio-2.png';
 import Portfolio3 from '../../assets/portfolio-3.png';
 import Portfolio4 from '../../assets/portfolio-4.png';
+import GitHub from '../../assets/github.png';
+import BlogLogo from '../../assets/bloglogo.png';
+import HappyCow from '../../assets/happy-cow.png';
 
 const Portfolio = () => {
   return (
@@ -11,9 +14,39 @@ const Portfolio = () => {
       <h2 className='pTitle'>My Projects</h2>
       <span className='worksSubTitle'>I invite you to explore my portfolio of projects and work, where you can freely browse and enjoy a curated collection showcasing my creative and professional endeavors.</span>
       <div className='worksImgs'>
-        <img src={Portfolio1} alt='project 1' className='worksImg' />
-        <img src={Portfolio2} alt='project 2' className='worksImg' />
-        <img src={Portfolio3} alt='project 3' className='worksImg' />
+        
+      <div className='port1'>
+  <a href="https://github.com/MadeleineA42/moody" target="_blank" rel="noopener noreferrer">
+    <img src={GitHub} alt="github icon" className="githubIcon" />
+  </a>
+  <a href="https://madeleinea42.github.io/moody/" target="_blank" rel="noopener noreferrer">
+    <img src={HappyCow} alt="github icon" className="happyCow" />
+  </a>
+  <img src={Portfolio1} alt='project 1' className='worksImg' />
+</div>
+
+
+<div className='port1'>
+  <div className='wrapper'><a href="https://github.com/newprice247/F2F-Blog" target="_blank" rel="noopener noreferrer">
+    <img src={GitHub} alt="github icon" className="githubIcon" />
+  </a>
+  <a href="https://f2f-blog-ad8f414d0b59.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+    <img src={BlogLogo} alt="github icon" className="bloglogo" />
+  </a>
+  </div>
+  <img src={Portfolio2} alt='project 1' className='worksImg' />
+</div>
+
+<div className='port1'>
+  <a href="https://github.com/kwaters3" target="_blank" rel="noopener noreferrer">
+    <img src={GitHub} alt="github icon" className="githubIcon" />
+  </a>
+  <a href="https://github.com/newprice247/F2F-Blog" target="_blank" rel="noopener noreferrer">
+    <img src={BlogLogo} alt="github icon" className="bloglogo" />
+  </a>
+  <img src={Portfolio3} alt='project 1' className='worksImg' />
+</div>
+
         <img src={Portfolio4} alt='project 4' className='worksImg' />
       </div>
       </section>
